1d5539144698ed7e010a0aeea81d52dce93bd3c8
rojinilboga <rojin@ilboga.nl>
2024-08-11 11:57:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Create a dictionary
+my_dict={
+    "France": "Paris",
+    "Netherlands": "Amsterdam",
+     "Spain": "Madrid",
+     "Italy": "Rome"
+}
+print(my_dict)
+
+#Access value from my_dict
+my_city=my_dict["Spain"]
+print("I want to visit ",my_city )
+
+#Create a list
+flowers=["Rose","Tulip","sunflower"]
+print(flowers)
